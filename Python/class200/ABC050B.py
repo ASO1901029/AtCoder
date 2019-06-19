1327415e1fc3aa8a2d40f25bbd3f7f9b593f7a13
ASO1701013 <YasInEgbofitVe9>
2019-06-20 00:16:00 +0900

    UPDATE:ABC050B

--- Python/class200/ABC050B.py
+++ Python/class200/ABC050B.py
@@ -1,9 +1,11 @@
-import sys
-N = sys.stdin.readline()
-T = map(int,sys.stdin.readline().split())
-M = sys.stdin.readline()
+N = input()
+T = list(map(int, input().split()))
+M = input()
+sum = sum(T) #合計を保持しておく
 PX = []
-for i in range(T):
-
-sum = sum(T)
-for i in PX:
+ansList = list()
+for i in range(int(M)):
+    PX.append(list(map(int, input().split())))
+for i in range(int(M)):
+    ansList.append(sum - (T[PX[i][0]-1] - PX[i][1])) #合計からジュースに対応するTの差分だけ減算する
+    print(ansList[i])
