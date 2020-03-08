6d9e03bd362d9c1f1001c8ac7e108d62e2218a07
Nakayama Reiya <nkymriy@gmail.com>
2020-03-08 22:30:01 +0900

    Add ABC153D

--- notCleared/ABC143D.py
+++ notCleared/ABC143D.py
@@ -8,12 +8,3 @@ N = int(input())
 L = list(map(int, input().split()))
 ans = 0
 L.sort(reverse=True)
-for i in range(len(L) - 2):
-    a = L[i]
-    for j in range(i + 1, len(L) - 1):
-        b = L[j]
-        upperC = a + b
-        lowerC = abs(a - b)
-        l_in = [i for i in L[j + 1:] if lowerC < i < upperC]
-        ans += len(l_in)
-print(ans)
