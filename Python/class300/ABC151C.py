1f91b720a0356f69521f0e7f6d94281844ef804f
Nakayama Reiya <nkymriy@gmail.com>
2020-03-11 01:16:09 +0900

    Update ABC151C

--- Python/class300/ABC151C.py
+++ Python/class300/ABC151C.py
@@ -1,16 +1,19 @@
-N,M = map(int,input().split())
-d = {}
+N, M = map(int, input().split())
+question = {}
+wa = {}
 ac_count = 0
 wa_count = 0
 for i in range(M):
-    q,s = input().split()
-    if not s in d.keys():
-        d[q] = ''
-    if d[q] == 'AC':
-        continue
-    d[q] = s
-    if s == 'WA':
-        wa_count += 1
+    q, s = input().split()
+    question.setdefault(q, '')
+    wa.setdefault(q, 0)
+    if s == 'AC':
+        if question[q] == 'AC':
+            continue
+        else:
+            question[q] = 'AC'
+            ac_count += 1
+            wa_count += wa[q]
     else:
-        ac_count += 1
-print(ac_count,wa_count)
+        wa[q] += 1
+print(ac_count, wa_count)
