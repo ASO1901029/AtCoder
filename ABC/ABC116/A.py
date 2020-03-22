cfb486f1a146967c9c881a87546283c0e0ea4af6
Nakayama Reiya <nkymriy@gmail.com>
2020-03-22 23:08:28 +0900

    Add ABC116A

---
+++ ABC/ABC116/A.py
@@ -0,0 +1,39 @@
+import sys
+from io import StringIO
+import unittest
+
+
+def resolve():
+    ls = list(map(int, input().split()))
+    ls.sort()
+    print(int(ls[0] * ls[1] * 0.5))
+
+
+class TestClass(unittest.TestCase):
+    def assertIO(self, input, output):
+        stdout, stdin = sys.stdout, sys.stdin
+        sys.stdout, sys.stdin = StringIO(), StringIO(input)
+        resolve()
+        sys.stdout.seek(0)
+        out = sys.stdout.read()[:-1]
+        sys.stdout, sys.stdin = stdout, stdin
+        self.assertEqual(out, output)
+
+    def test_入力例_1(self):
+        input = """3 4 5"""
+        output = """6"""
+        self.assertIO(input, output)
+
+    def test_入力例_2(self):
+        input = """5 12 13"""
+        output = """30"""
+        self.assertIO(input, output)
+
+    def test_入力例_3(self):
+        input = """45 28 53"""
+        output = """630"""
+        self.assertIO(input, output)
+
+
+if __name__ == "__main__":
+    unittest.main()
