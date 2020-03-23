01eebb41d1f3e4bb34b9c2b8d7c6fa6fa2cdb5d4
Nakayama Reiya <nkymriy@gmail.com>
2020-03-24 07:09:36 +0900

    Add ABC120 A

--- ABC/ABC120/A.py
+++ ABC/ABC120/A.py
@@ -1,2 +1,15 @@
-A,B,C = map(int,input().split())
-print(min(B//A,C))
+def main():
+    from builtins import int, map, list, print, min
+    import sys
+    sys.setrecursionlimit(10 ** 6)
+
+    input = sys.stdin.readline
+    input_list = (lambda: input().rstrip().split())
+    input_number = (lambda: int(input()))
+    input_number_list = (lambda: list(map(int, input_list())))
+
+    A,B,C = input_number_list()
+    print(min(B//A,C))
+
+if __name__ == '__main__':
+    main()
