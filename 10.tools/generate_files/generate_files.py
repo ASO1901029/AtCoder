c09edfedbd473c481c93c37f33681ee18892735a
Nakayama Reiya <nkymriy@gmail.com>
2020-03-24 03:48:45 +0900

    Add create of other-contest on generate_files.py

--- 10.tools/generate_files/generate_files.py
+++ 10.tools/generate_files/generate_files.py
@@ -11,35 +11,38 @@ class GenerateFiles:
         # print(r.text)
         contest_name = contest[0:3]
         contest_name = contest_name.upper()
-        folder = f'../../{contest_name}/{contest}/'
         if contest_name in ['ABC', 'ARC', 'AGC']:
-            use_template = False
-            if os.path.isfile(template):
-                print('テンプレートを使用します')
-                use_template = True
-            print(f'{folder}を作成します。よろしいですか？[Y/N]')
-            yn = input()
-            if yn != 'Y':
-                print('終了します')
-                exit()
-            # フォルダが無い場合は作成する
-            if os.path.exists(folder):
-                print(f'既にフォルダが存在しています({folder})')
+            folder = f'../../{contest_name}/{contest}/'
+        else:
+            folder = f'../../others/{contest}/'
+
+        use_template = False
+        if os.path.isfile(template):
+            print('テンプレートを使用します')
+            use_template = True
+        print(f'{folder}を作成します。よろしいですか？[Y/N]')
+        yn = input()
+        if yn != 'Y':
+            print('終了します')
+            exit()
+        # フォルダが無い場合は作成する
+        if os.path.exists(folder):
+            print(f'既にフォルダが存在しています({folder})')
+        else:
+            os.mkdir(folder)
+            print(f'{folder}を作成しました')
+        for c in ['A', 'B', 'C', 'D', 'E', 'F']:
+            file_path = f'{folder}{c}.py'
+            if os.path.exists(file_path):
+                print(f'{c}:ファイルが既に存在しています')
             else:
-                os.mkdir(folder)
-                print(f'{folder}を作成しました')
-            for c in ['A', 'B', 'C', 'D', 'E', 'F']:
-                file_path = f'{folder}{c}.py'
-                if os.path.exists(file_path):
-                    print(f'{c}:ファイルが既に存在しています')
+                if use_template:
+                    shutil.copy2(template,file_path)
+                    print(f'{folder}{c}.py を生成しました')
                 else:
-                    if use_template:
-                        shutil.copy2(template,file_path)
-                        print(f'{folder}{c}.py を生成しました')
-                    else:
-                        f = open(file_path, 'w')
-                        f.close()
-                        print(f'{folder}{c}.py を生成しました')
+                    f = open(file_path, 'w')
+                    f.close()
+                    print(f'{folder}{c}.py を生成しました')
 
 
 if __name__ == '__main__':
